4f8af49da8a97f728dce3298c20f629f2d2bf1f8
MacsinoiuElena <venera.elena99@yahoo.com>
2021-01-05 14:31:34 +0200

    last version

--- app/controllers/Students.php
+++ app/controllers/Students.php
@@ -8,6 +8,11 @@ class Students extends Controller{
 
     }
     
+    public function index(){
+        
+        $data = ['title' => 'Proiecte Electronica', 'description1' => 'Fiecare student isi va alege un singur proiect', 'description2' => 'Termenul limita pentru predare va fi peste 2 saptamani'];
+        $this->view('pages/index', $data);
+    }
 
     public function studenti(){
         $data = [
